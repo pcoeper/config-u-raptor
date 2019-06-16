eb6ad43edf34c902ba0dbb994e028d4fd09fe95f
Patrick Cöper <p.coeper@salt-and-pepper.eu>
2019-06-16 17:21:51 +0200

    simplify saveOrUpdate call for config parameters

--- src/db/controller/ConfigParameterController.ts
+++ src/db/controller/ConfigParameterController.ts
@@ -13,48 +13,21 @@ export class ConfigParameterController {
     const parameterRepo = getRepository(ConfigParameter);
     return new Promise(async (resolve, reject) => {
       for (const p of parameters) {
-        let saveParam = false;
-
         // avoid undefined id when p is new instance
         p.id = p.id ? p.id : 0;
 
         let parameter = await parameterRepo.findOne(p.id);
 
-        // parameter already exists -> update?
-        if (parameter) {
-          if (parameter.name !== p.name) {
-            parameter.name = p.name;
-            saveParam = true;
-          }
-
-          if (parameter.type !== p.type) {
-            parameter.type = p.type;
-            saveParam = true;
-          }
-
-          if (parameter.defaultValue !== p.defaultValue) {
-            parameter.defaultValue = p.defaultValue;
-            saveParam = true;
-          }
-
-          if (parameter.description !== p.description) {
-            parameter.description = p.description;
-            saveParam = true;
-          }
-        } else {
+        if (!parameter) {
           // create new parameter
           parameter = new ConfigParameter();
-          parameter.name = p.name;
-          parameter.type = p.type;
-          parameter.defaultValue = p.defaultValue;
-          parameter.description = p.description;
-
-          saveParam = true;
         }
+        parameter.name = p.name;
+        parameter.type = p.type;
+        parameter.defaultValue = p.defaultValue;
+        parameter.description = p.description;
 
-        if (saveParam) {
-          await parameterRepo.save(parameter);
-        }
+        await parameterRepo.save(parameter);
       }
       // resolve promise when done
       resolve(true);
