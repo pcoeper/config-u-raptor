8ad37fad9618a163ae78bdb1613972a617d52db4
Patrick Cöper <schmidtp_mail@yahoo.de>
2019-09-29 17:12:42 +0200

    removed unused packages from package.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import Vue from "vue";
 
 export default Vue.extend({});
 </script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -5,8 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator';
-
+import Vue from "vue";
 export default Vue.extend({});
 </script>
 
